7d2de1d3f51e3f7e30d612c84aff3262224c833c
imran <imran>
2023-10-07 12:27:19 +0530

    Created New Project

---
+++ src/main/java/com/botbel/botpay/BotpayApplication.java
@@ -0,0 +1,13 @@
+package com.botbel.botpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotpayApplication.class, args);
+	}
+
+}
